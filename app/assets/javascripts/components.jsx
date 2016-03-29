24b1d0c12b0d99b43d10c9c525791c4cc2f23747
gtalkowski <g.talkowski@gmail.com>
2016-03-29 10:47:21 +0200

    Add rendering

---
+++ app/assets/javascripts/components.jsx
@@ -0,0 +1,17 @@
+
+class Main extends React.Component {
+  render(){
+    return (
+        <h1>Hello from the component</h1>
+        );
+  }
+}
+
+//let documentReady = () => {
+//  ReactDOM.render(
+//      <Main />,
+//      document.getElementById('react')
+//      );
+//};
+
+$(documentReady);
